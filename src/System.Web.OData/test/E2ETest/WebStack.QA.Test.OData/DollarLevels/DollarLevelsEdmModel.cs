8a9df786c3b2543d20122d570727dcf3a2000101
Mitchell van Lee <mitchell_willem2@hotmail.com>
2018-01-10 15:52:16 +0100

    Added System.Web.OData.

---
+++ src/System.Web.OData/test/E2ETest/WebStack.QA.Test.OData/DollarLevels/DollarLevelsEdmModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.OData.Builder;
+using Microsoft.OData.Edm;
+
+namespace WebStack.QA.Test.OData.DollarLevels
+{
+    public class DollarLevelsEdmModel
+    {
+        public static IEdmModel GetConventionModel()
+        {
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<DLManager>("DLManagers");
+            builder.EntitySet<DLEmployee>("DLEmployees");
+
+            builder.Namespace = typeof(DLManager).Namespace;
+            return builder.GetEdmModel();
+        }
+    }
+}
